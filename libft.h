7796ea8cd86bc48422e4e52c4acee045f3cfc03f
lea medrano <leamedrano@gmail.com>
2022-10-17 13:56:10 +0200

    adding headers

---
+++ libft.h
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmedrano <lmedrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 16:07:49 by lmedrano          #+#    #+#             */
+/*   Updated: 2022/10/17 13:50:48 by lmedrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void *ft_memmove(void *dst, const void *src, size_t nbytes);
+void *ft_memcpy(void *dst, const void *src, size_t nbytes);
