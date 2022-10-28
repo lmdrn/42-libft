c0663d0584939f50590beaad8c87a6cb89f85a44
Lea Medrano <lmedrano@c1r11s6.42lausanne.ch>
2022-10-28 14:56:35 +0200

    split progress

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,12 @@
 /*   By: lmedrano <lmedrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:41:45 by lmedrano          #+#    #+#             */
-/*   Updated: 2022/10/26 19:54:57 by lmedrano         ###   ########.fr       */
+/*   Updated: 2022/10/28 14:56:02 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlen(char const *s)
 {
@@ -40,41 +41,30 @@ int	ft_how_many_sep(char const *s, char c)
 	return (flag);
 }
 
-int	ft_array_elem_count(char const *s, int sep_count)
-{
-	int	elem_count;
-
-	elem_count = (ft_strlen(s) / sep_count);
-	return (elem_count);
-}
-
 char	**ft_split(char const *s, char c)
 {
-	int	len;
+	int		len;
 	int 	sep_count;
 	int 	i;
-	int	j;
-	int	k;
+	int		j;
+	int		k;
 	char	**tab;
 
 	i = 0;
 	j = 0;
 	k = 0;
 	sep_count = ft_how_many_sep(s, c);
-	len = ft_array_elem_count(s, sep_count);
-	if (!s || !c)
-		return (NULL);
-	tab = malloc(sizeof(char) * (len + 1));
+	len = ft_strlen(s);
+	tab = malloc(sizeof(*tab) * (len + 1));
 	if (tab == NULL)
 		return (NULL);
 	while (s[i] != '\0')
 	{
+		tab[j] = malloc(sizeof(*tab[j]) * (len + 1));
 		k = 0;
-		while (!(s[i] == c))
+		while (s[i] != c)
 		{
-			tab[j][k] = s[i];
-			i++;
-			k++;
+			tab[j][k++] = s[i++];
 		}
 		j++;
 		i++;
@@ -82,21 +72,9 @@ char	**ft_split(char const *s, char c)
 	return (tab);
 }
 
-
-// check cmb de separators
-// check how many element in array
-// allocate new str with length of array - separators
-// iterate on s and populate new str with each elem of the array
-// Careful it's two dimensions....
-//
-// "he ha ho"
-// "he", "ha", "ho";
-// tester les valeurs de retours de sepcount + elem_array_count avec printf
-//
-#include <stdio.h>
-
-int main(void)
+int main (void)
 {
-	printf("%d", ft_how_many_sep("he ha ho", ' '));
-	printf("%d", ft_array_elem_count("he ha ho", ft_how_many_sep("he ha ho", ' ')));
+	printf("%s", ft_split("hello lea", ' ')[0]);
+	printf("\n");
+	printf("%s", ft_split("hello lea", ' ')[1]);
 }
