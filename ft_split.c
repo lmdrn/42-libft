1520a82c644a5317b583f62df1bb98a4936b9762
Lea Medrano <lmedrano@c1r11s4.42lausanne.ch>
2022-10-29 17:50:54 +0200

    itoa + split

--- ft_split.c
+++ ft_split.c
@@ -1,80 +1,94 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmedrano <lmedrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:41:45 by lmedrano          #+#    #+#             */
-/*   Updated: 2022/10/28 14:56:02 by lmedrano         ###   ########.fr       */
+/*   Updated: 2022/10/29 13:13:46 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-size_t	ft_strlen(char const *s)
+int	ft_word_count(char const *s, char c)
 {
 	int	i;
+	int word;
 
 	i = 0;
-	while (s[i])
-		i++;
-	return(i);
-}
-
-int	ft_how_many_sep(char const *s, char c)
-{
-	int 	i;
-	int	flag;
-
-	i = 0;
-	flag = 0;
-	if (s == NULL)
-		return (0);
+	word = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			flag++;
-		i++;
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i] != '\0')
+					i++;
+			word++;
+		}
+		while (s[i] == c)
+			i++;
 	}
-	return (flag);
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		len;
-	int 	sep_count;
-	int 	i;
+	int		i;
 	int		j;
 	int		k;
+	int		count;
 	char	**tab;
 
 	i = 0;
 	j = 0;
-	k = 0;
-	sep_count = ft_how_many_sep(s, c);
-	len = ft_strlen(s);
-	tab = malloc(sizeof(*tab) * (len + 1));
+	tab = malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
 	if (tab == NULL)
 		return (NULL);
-	while (s[i] != '\0')
+	while (j < (ft_word_count(s, c)))
 	{
-		tab[j] = malloc(sizeof(*tab[j]) * (len + 1));
-		k = 0;
-		while (s[i] != c)
+		count = 0;
+		while (s[i] == c)
+				i++;
+		while (s[i] != c && s[i] != '\0')
 		{
-			tab[j][k++] = s[i++];
+			count++;
+			i++;
 		}
+		tab[j] = malloc(sizeof(char) * (count + 1));
+		if (tab[j] == NULL)
+		{
+			while (j >= 0)
+			{
+				free(tab[--j]);
+			}
+			free(tab);
+			return (NULL);
+		}
+		i -= count;
+		k = 0;	
+		while (s[i] != c && s[i] != '\0')
+			tab[j][k++] = s[i++];
+		tab[j][k] = '\0';
 		j++;
-		i++;
 	}
+	tab[j] = NULL;
 	return (tab);
 }
-
+/*
+#include <stdio.h>
 int main (void)
 {
-	printf("%s", ft_split("hello lea", ' ')[0]);
-	printf("\n");
-	printf("%s", ft_split("hello lea", ' ')[1]);
+	char	str[] = " Hello Lea";
+	char	**tab = ft_split(str, ' ');
+	int		i;
+
+	i = 0;
+	printf("%d\n", ft_word_count(str, ' '));
+	while (i < ft_word_count(str, ' '))
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	return (0);
 }
+*/
